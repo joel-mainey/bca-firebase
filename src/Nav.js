9c9de7d1c99fd20eb7d949494a7ebebd643c9941
joel-mainey <grunty8848@gmail.com>
2023-03-30 15:08:51 +1300

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,33 @@
+import {Link} from 'react-router-dom';
+
+function Nav()
+{
+    return(
+        <nav className="navbar p-3 rounded border" style={{backgroundColor: "rgba(0,0,0,0.92)"}}>
+            <h3 className="Display-3 navbar-brand text-white"><img src="/images/bcalogo.png" alt="Logo" style={{"max-width" : "45px", "height" : "auto" }}></img> BCA Biodiversity</h3>
+            <ul className="nav nav-pills navbar-right">
+                <Link to="/" className='nav-link'>
+                    <li className="nav-item text-white p-3"><h3>Home</h3></li>
+                </Link>
+                <Link to="/About" className='nav-link'>
+                    <li className="nav-item text-white p-3"><h3>About</h3></li>
+                </Link>
+                <Link to="/Nursery" className='nav-link'>
+                    <li className="nav-item text-white p-3"><h3>Nursery</h3></li>
+                </Link>
+                <Link to="/Hilltop" className='nav-link'>
+                    <li className="nav-item text-white p-3"><h3>Hilltop</h3></li>
+                </Link>
+                <Link to="/Valley" className='nav-link'>
+                    <li className="nav-item text-white p-3"><h3>Valley</h3></li>
+                </Link>
+                <Link to="/Wetlands" className='nav-link'>
+                    <li className="nav-item text-white p-3"><h3>Wetlands</h3></li>
+                </Link>
+            </ul>
+
+        </nav>
+    )
+}
+
+export default Nav;
---
+++ src/Valley.js
@@ -0,0 +1,49 @@
+// Importing needed webhooks.
+import {useEffect, useState} from 'react';
+import { useHref } from 'react-router-dom';
+
+
+function Valley()
+{
+    //
+    //
+    //Add const variables here
+//     const [state, setState] = useState([])
+
+//     function getData()
+//     {
+//     //API data from restDB
+//     var data = null;
+
+// var xhr = new XMLHttpRequest();
+// xhr.withCredentials = false;
+
+// xhr.addEventListener("readystatechange", function () {
+//   if (this.readyState === 4) {
+//     console.log(this.responseText);
+//   }
+// });
+
+// xhr.open("GET", "https://bcaspecieslist-bbda.restdb.io/rest/specieslist");
+// xhr.setRequestHeader("content-type", "application/json");
+// xhr.setRequestHeader("x-apikey", "49d7afd2dfe76921b78d77960e8828d02c273");
+// xhr.setRequestHeader("cache-control", "no-cache");
+
+// xhr.send(data);
+//     }
+
+//     //Using the useEffect webhook we can now tell the DOM to display the data
+//     //First parameter is a function to grab data, second is to update state variable with data.
+//     useEffect
+//     (
+//         ()=>{
+//             getData();
+//         }, [state]
+//     );
+
+
+
+    
+}
+
+export default Valley();
