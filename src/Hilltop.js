920580e38dab634e86b5ff8930175676f0c55da0
Hugh White <30012235@student.toiohomai.ac.nz>
2023-06-01 13:52:47 +1200

    changing card id from plant id back to plant name without spaces

--- src/Hilltop.js
+++ src/Hilltop.js
@@ -84,7 +84,7 @@ function Hilltop() {
         <div
           className="card-body mt-3 p-3 rounded border shadow text-white"
           style={{ backgroundColor: "rgba(0,0,0,0.92)" }}
-          id={plant.id}
+          id={plant.Name.replace(/\s/g, "")} // Remove spaces from plant name to create the card ID
           key={plant.Name}
         >
           <h1>{plant.Name}</h1>
